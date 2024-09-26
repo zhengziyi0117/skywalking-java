827819bccbf70148a61496c84ff70e57678f6117
zhengziyi <zhengziyi0117@gmail.com>
2024-09-26 20:59:13 +0800

    fix: code issue

--- apm-sniffer/apm-agent-core/src/main/java/org/apache/skywalking/apm/agent/core/asyncprofiler/AsyncProfilerTask.java
+++ apm-sniffer/apm-agent-core/src/main/java/org/apache/skywalking/apm/agent/core/asyncprofiler/AsyncProfilerTask.java
@@ -26,7 +26,6 @@ import org.apache.skywalking.apm.util.StringUtil;
 
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
